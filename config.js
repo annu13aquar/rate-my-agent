2b95affda9a8705d78f083a2928e96a22e86595f
annu <ann13aquarius@gmail.com>
2017-10-09 01:28:57 +1100

    adding es6 capability

--- config.js
+++ config.js
@@ -2,6 +2,12 @@ exports.config = {
 
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
-  specs: ['searchFunctionality/*/*.js'],
-  
+  jasmineNodeOpts: {
+  defaultTimeoutInterval: 180000,
+  },
+  specs: ['searchFunctionality/*.js'],
+  params: {
+    URL: 'https://www.ratemyagent.com.au/'
+  }
+
 };
---
+++ searchFunctionality/searchByAgentName.js
@@ -0,0 +1,13 @@
+describe('Search by a agent name', () => {
+  "use strict";
+
+  it('should return matching location names, agent and agent names', () =>{
+
+    browser.get(browser.params.URL);
+
+});
+
+afterAll(function(){
+    browser.quit();
+  });
+});
